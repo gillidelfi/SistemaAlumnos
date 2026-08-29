ad6239aecd4d101eeaae5c038c44643b33d00582
gillidelfi <gillidelfi@gmail.com>
2026-08-29 15:16:05 -0300

    etapa1: la primera clase

---
+++ SistemaAlumnos/Program.cs
@@ -0,0 +1,21 @@
+﻿using SistemaAlumnos;
+
+Alumno alumno1 = new Alumno();
+alumno1.Nombre = "Sara";
+alumno1.Legajo = 22;
+alumno1.Nota1 = 8;
+alumno1.Nota2 = 10;
+
+Alumno alumno2 = new Alumno();
+alumno2.Nombre = "Bautista";
+alumno2.Legajo = 24;
+alumno2.Nota1 = 7;
+alumno2.Nota2 = 9;
+
+Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
+Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
+
+alumno1.Nombre = "Emma";
+
+Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
+Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");

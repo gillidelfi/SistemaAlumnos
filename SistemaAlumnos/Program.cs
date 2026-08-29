5e627ac3da976b169352445f811718efff00f7fb
gillidelfi <gillidelfi@gmail.com>
2026-08-29 15:25:31 -0300

    etapa2: Constructor

--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -1,16 +1,8 @@
 ﻿using SistemaAlumnos;
 
-Alumno alumno1 = new Alumno();
-alumno1.Nombre = "Sara";
-alumno1.Legajo = 22;
-alumno1.Nota1 = 8;
-alumno1.Nota2 = 10;
+Alumno alumno1 = new Alumno("Sara",22, 8,10);
 
-Alumno alumno2 = new Alumno();
-alumno2.Nombre = "Bautista";
-alumno2.Legajo = 24;
-alumno2.Nota1 = 7;
-alumno2.Nota2 = 9;
+Alumno alumno2 = new Alumno("Bautista", 24, 7, 9);
 
 Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
@@ -19,3 +11,11 @@ alumno1.Nombre = "Emma";
 
 Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
+
+
+/* ejemplo de error: Alumno alumno3 = new Alumno();
+ salida por consola:0>Program.cs(14,22): Error CS7036 : No se ha dado ningún argumento que corresponda al parámetro requerido "nombre" de "Alumno.Alumno(string, int, decimal, decimal)"
+   0>------- Finished building project: SistemaAlumnos. Succeeded: False. Errors: 1. Warnings: 0
+   Build completed in 00:00:03.343 
+   explicacion: una vez que se crea un constructor personalizado, el constructor por defecto ya no funciona. Por eso new Alumno() sin argumentos ya no compila: le falta lo que el constructor exige.*/
+

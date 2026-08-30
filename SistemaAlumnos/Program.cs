710441511adf822af64bf65be7a03c9cd8915bd5
gillidelfi <gillidelfi@gmail.com>
2026-08-30 10:12:03 -0300

    Etapa 3: métodos

--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -12,6 +12,16 @@ alumno1.Nombre = "Emma";
 Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
 
+Console.WriteLine($"{alumno1.Nombre} - Promedio: {alumno1.Promedio()} - Aprobado: {alumno1.EstaAprobado()}");
+Console.WriteLine($"{alumno2.Nombre} - Promedio: { alumno2.Promedio()} - Aprobado: {alumno2.EstaAprobado()}");
+
+// Prueba subirNota()
+alumno2.SubirNota();
+Console.WriteLine($"Después de SubirNota : {alumno2.Nombre} - Nota1: {alumno2.Nota1} - Nota2: {alumno2.Nota2} - Promedio: {alumno2.Promedio()} - Aprobado: {alumno2.EstaAprobado()}");
+
+Alumno alumnoTope = new Alumno("Sofía", 103, 10, 9.5m);
+alumnoTope.SubirNota();
+Console.WriteLine($"{alumnoTope.Nombre} - Nota1: {alumnoTope.Nota1} - Nota2: {alumnoTope.Nota2}");
 
 /* ejemplo de error: Alumno alumno3 = new Alumno();
  salida por consola:0>Program.cs(14,22): Error CS7036 : No se ha dado ningún argumento que corresponda al parámetro requerido "nombre" de "Alumno.Alumno(string, int, decimal, decimal)"

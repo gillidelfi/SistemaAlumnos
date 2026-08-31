3884558a82450fc683401d9ec7acd27d0bf3f486
gillidelfi <gillidelfi@gmail.com>
2026-08-31 12:36:21 -0300

    Etapa 5: Encapsulamiento

--- SistemaAlumnos/Alumno.cs
+++ SistemaAlumnos/Alumno.cs
@@ -3,16 +3,14 @@ namespace SistemaAlumnos;
 public class Alumno
 {
     public string Nombre { get; set; }
-    public int Legajo { get; set; }
-    public decimal Nota1 { get; set; }
-    public decimal Nota2 { get; set; }
+    public int Legajo { get; private set; }
+    public decimal Nota1 { get; private set; }
+    public decimal Nota2 { get; private set; }
 
-    public Alumno(string nombre, int legajo, decimal nota1, decimal nota2)
+    public Alumno(string nombre, int legajo) //metodo constructor 
     {
         Nombre = nombre; 
         Legajo = legajo;
-        Nota1 = nota1;
-        Nota2 = nota2;
     }
 
     public decimal Promedio()
@@ -44,7 +42,19 @@ public class Alumno
     {
         return $" {Legajo} -  {Nombre} - {Promedio()}";
     }
+
+    public bool CargarNotas(decimal nota1, decimal nota2)
+    {
+        if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10)
+        {
+            return false; // si las notas no se encuentran en el rango, devuelve false 
+        }
+
+        Nota1 = nota1;
+        Nota2 = nota2;
+        return true; // si se encuentran, las asigna y devuelve true 
+    }
+    }
     /* mensaje al sacar override: Warning CS0114 : 'Alumno.ToString()' oculta el miembro heredado 'object.ToString()'. Para hacer que el miembro actual invalide esa implementación, agregue la palabra clave override. Si no, agregue la palabra clave new.
        SistemaAlumnos -> /Users/delfinagilli/RiderProjects/SistemaAlumnos/SistemaAlumnos/bin/Debug/net10.0/SistemaAlumnos.dll
        0>------- Finished building project: SistemaAlumnos. Succeeded: True. Errors: 0. Warnings: 1*/
-}
--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -1,16 +1,22 @@
 ﻿using SistemaAlumnos;
 
-Alumno alumno1 = new Alumno("Sara",22, 8,10);
+Alumno alumno1 = new Alumno("Sara",22);
 
-Alumno alumno2 = new Alumno("Bautista", 24, 7, 9);
+Alumno alumno2 = new Alumno("Bautista", 24);
 
 Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
 
 alumno1.Nombre = "Emma";
+// alumno1.Nota1 = 47; ya no compila porque Nota1 esta en private set
+
 
 Console.WriteLine($"{alumno1.Nombre} --- {alumno1.Legajo}");
 Console.WriteLine($"{alumno2.Nombre} --- {alumno2.Legajo}");
+
+// Cargar notas antes del promedio
+alumno1.CargarNotas(5, 8);
+alumno2.CargarNotas(7, 9);
 
 Console.WriteLine($"{alumno1.Nombre} - Promedio: {alumno1.Promedio()} - Aprobado: {alumno1.EstaAprobado()}");
 Console.WriteLine($"{alumno2.Nombre} - Promedio: { alumno2.Promedio()} - Aprobado: {alumno2.EstaAprobado()}");
@@ -19,7 +25,7 @@ Console.WriteLine($"{alumno2.Nombre} - Promedio: { alumno2.Promedio()} - Aprobad
 alumno2.SubirNota();
 Console.WriteLine($"Después de SubirNota : {alumno2.Nombre} - Nota1: {alumno2.Nota1} - Nota2: {alumno2.Nota2} - Promedio: {alumno2.Promedio()} - Aprobado: {alumno2.EstaAprobado()}");
 
-Alumno alumnoTope = new Alumno("Sofía", 103, 10, 9.5m);
+Alumno alumnoTope = new Alumno("Sofía", 103);
 alumnoTope.SubirNota();
 Console.WriteLine($"{alumnoTope.Nombre} - Nota1: {alumnoTope.Nota1} - Nota2: {alumnoTope.Nota2}");
 
@@ -29,3 +35,13 @@ Console.WriteLine($"{alumnoTope.Nombre} - Nota1: {alumnoTope.Nota1} - Nota2: {al
    Build completed in 00:00:03.343 
    explicacion: una vez que se crea un constructor personalizado, el constructor por defecto ya no funciona. Por eso new Alumno() sin argumentos ya no compila: le falta lo que el constructor exige.*/
 
+bool CargaNota = alumno1.CargarNotas(5, 22);
+
+if (CargaNota)
+{
+    Console.WriteLine("Notas cargadas correctamente");
+}
+else
+{
+    Console.WriteLine("Notas inválidas: no fueron cargadas ");
+}

0a32773af2693914cc1382cc1307031903c2aa97
gillidelfi <gillidelfi@gmail.com>
2026-09-07 10:19:58 -0300

    Etapa 6: Muchos objetos

--- SistemaAlumnos/Program.cs
+++ SistemaAlumnos/Program.cs
@@ -45,3 +45,143 @@ else
 {
     Console.WriteLine("Notas inválidas: no fueron cargadas ");
 }
+//ETAPA 6: 
+
+List<Alumno> alumnos = new List<Alumno>();
+int opcion;
+bool opcionValida;
+
+do
+{
+    Console.WriteLine(" MENU DE OPCIONES");
+    Console.WriteLine("1. Agregar alumno");
+    Console.WriteLine("2. Listar alumnos");
+    Console.WriteLine("3. Buscar alumno por legajo");
+    Console.WriteLine("4. Promedio general del curso");
+    Console.WriteLine("5. Cantidad de alumnos aprobados");
+    Console.WriteLine("6. Salir");
+    Console.Write("Elegí una opción: ");
+    
+    opcion = int.Parse( Console.ReadLine());
+    opcionValida = opcion == 1 || opcion == 2 || opcion == 3 || opcion == 4  || opcion == 5 || opcion == 6;
+    if (!opcionValida)
+    {
+        Console.WriteLine("Opción inválida. Intente de nuevo.");
+
+    }
+    switch (opcion)
+    {
+        case 1:
+            AgregarAlumno(alumnos);
+            break;
+        case 2:
+            ListarAlumnos(alumnos);
+            break;
+        case 3:
+            BuscarAlumno(alumnos);
+            break;
+        case 4:
+            PromedioGeneral(alumnos);
+            break;
+        case 5:
+            CantidadAprobados(alumnos);
+            break;
+        default:
+            Console.WriteLine("Opción inválida. Elegí un número del 1 al 6.");
+            break;
+    }
+} while (opcion != 6);
+void AgregarAlumno(List<Alumno> lista)
+{
+    Console.Write("Nombre: ");
+    string nombre = Console.ReadLine();
+
+    Console.Write("Legajo: ");
+    int legajo = int.Parse(Console.ReadLine());
+    
+    Console.Write("Nota 1: ");
+    decimal nota1 = decimal.Parse(Console.ReadLine());
+
+    Console.Write("Nota 2: ");
+    decimal nota2 = decimal.Parse(Console.ReadLine());
+    
+    Alumno nuevo = new Alumno(nombre, legajo);
+    lista.Add(nuevo);
+    Console.WriteLine("Alumno agregado.");
+    
+    bool notacargada = nuevo.CargarNotas(nota1, nota2);
+    if (!notacargada)
+    {
+        Console.WriteLine("Las notas deben estar entre 0 y 10. Se agregó el alumno sin notas cargadas.");
+    }
+}
+
+void ListarAlumnos(List<Alumno> lista)
+{
+    if (lista.Count == 0)
+    {
+        Console.WriteLine("No hay Alumnos cargados.");
+        return;
+    }
+
+    foreach (Alumno a in lista)
+    {
+        Console.WriteLine(a.ToString());
+    }
+}
+
+void BuscarAlumno(List<Alumno> lista)
+{
+    Console.Write("Ingrese el legajo del alumno: ");
+    int legajo = int.Parse(Console.ReadLine());
+
+    Alumno encontrado = null;
+    foreach (Alumno a in lista)
+    {
+        if (a.Legajo == legajo)
+        {
+            encontrado = a;
+            break; 
+        }
+    }
+    if (encontrado == null)
+    {
+        Console.WriteLine("No se encontraron alumnos con ese legajo.");
+    }
+    else
+    {
+        Console.WriteLine(encontrado.ToString());
+    }
+        
+}
+
+void PromedioGeneral(List<Alumno> lista)
+{
+    if (lista.Count == 0)
+    {
+        Console.WriteLine("No hay alumnos cargados todavía.");
+        return;
+    }
+    decimal suma = 0;
+    foreach (Alumno a in lista)
+    {
+       suma += a.Promedio(); 
+    }
+    decimal promedioGeneral = suma / lista.Count;
+    Console.WriteLine($"El promedio general del curso: {promedioGeneral}");
+}
+
+void CantidadAprobados(List<Alumno> lista)
+{
+    int aprobados = 0;
+    foreach (Alumno a in lista)
+    {
+        if (a.EstaAprobado())
+        {
+            aprobados++; 
+        }
+    }
+    Console.WriteLine($"Cantidad de alumnos aprobados: {aprobados}");
+}
+
+
